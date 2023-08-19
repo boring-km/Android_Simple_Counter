a22e8d25a15683936a7bff29890468e173e94151
boring-km <kms0644804@naver.com>
2023-08-19 23:19:17 +0900

    [master/fix] add compose dependency again

--- app/src/main/java/com/boring/gangmin/counter/Counter.kt
+++ app/src/main/java/com/boring/gangmin/counter/Counter.kt
@@ -1,21 +1,26 @@
 package com.boring.gangmin.counter
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.CombinedVibration
+import android.os.VibrationEffect
 import android.os.Vibrator
+import android.os.VibratorManager
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.boring.gangmin.counter.databinding.ActivityMainBinding
-import java.lang.NumberFormatException
 
 class Counter : AppCompatActivity() {
 
     private var pressTime: Long = 0
     private var number: CountNumber = CountNumber(0)
     private lateinit var binding: ActivityMainBinding
+    private lateinit var backPressedCallback: OnBackPressedCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,22 @@ class Counter : AppCompatActivity() {
         setPlusButtonClickListener()
         setMinusButtonClickListener()
         setResetButtonClickListener()
+
+        setBackPressedCallback()
+    }
+
+    private fun setBackPressedCallback() {
+        backPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (System.currentTimeMillis() - pressTime < 2000) {
+                    finishAffinity()
+                    return
+                }
+                Toast.makeText(applicationContext, "한 번 더 취소하시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+                pressTime = System.currentTimeMillis()
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, backPressedCallback)
     }
 
     private fun setResetButtonClickListener() {
@@ -48,10 +69,29 @@ class Counter : AppCompatActivity() {
         }
     }
 
-    @Suppress("DEPRECATION")
+    private val vibratorManager by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+        } else {
+            @Suppress("DEPRECATION")
+            getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
+    }
+
     private fun setPlusButtonClickListener() {
         binding.plusButton.setOnClickListener {
-            (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).vibrate(3)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                (vibratorManager as VibratorManager).vibrate(
+                    CombinedVibration.createParallel(
+                        VibrationEffect.createOneShot(3, VibrationEffect.DEFAULT_AMPLITUDE)
+                    )
+                )
+            } else {
+                @Suppress("DEPRECATION")
+                (vibratorManager as Vibrator).vibrate(3)
+            }
+
             if (isIntNumber()) {
                 getNotEmptyCountNumber()
                 number.setNumber(binding.countNumber.text)
@@ -89,7 +129,8 @@ class Counter : AppCompatActivity() {
     }
 
     private fun getInputMethodManager(): InputMethodManager {
-        val inputMethodManager: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager: InputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         binding.countNumber.imeOptions = EditorInfo.IME_ACTION_DONE
         setCountNumber()
         return inputMethodManager
@@ -99,18 +140,16 @@ class Counter : AppCompatActivity() {
         binding.countNumber.setText(number.number.toString())
     }
 
-    override fun onBackPressed() {
-        if (System.currentTimeMillis() - pressTime < 2000) {
-            finishAffinity()
-            return
-        }
-        Toast.makeText(this, "한 번 더 취소하시면 종료됩니다.", Toast.LENGTH_SHORT).show()
-        pressTime = System.currentTimeMillis()
-    }
 
     override fun onStop() {
         super.onStop()
-        (getSystemService(Context.VIBRATOR_SERVICE) as Vibrator).cancel()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            (vibratorManager as VibratorManager).cancel()
+        } else {
+            (vibratorManager as Vibrator).cancel()
+        }
+
         saveNumber()
     }
 
